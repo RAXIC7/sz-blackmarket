cb905cb3717f0d9a9f0d7c533991e680a198bfec
StunZ <115426921+ItsStunZ@users.noreply.github.com>
2023-06-30 06:11:22 +0100

    Update config.lua
    Choose between inventory or menu to open shop

--- sz-blackmarket/shared/config.lua
+++ sz-blackmarket/shared/config.lua
@@ -5,6 +5,8 @@ Config.Debug = false
 Config.OpenHour = 1
 Config.CloseHour = 5 
 
+Config.UseMenu = true -- Set to true if you want to use qb-menu instead of inventory
+
 Config.BlackListedJobs = { -- Add jobs you dont want using the black market
     'police',
     'ambulance'
@@ -15,8 +17,12 @@ Config.DoorLocations = {
     -- ['2'] = {name = 'Rancho', location = vector3(420.41, -2064.29, 22.14)} -- Add more locations if you want to
 }
 
-Config.Items = {
-    ['1'] = {item = 'advancedlockpick', header = 'Advanced Lockpick', price = 100, description = '?', icon = 'fas fa-screwdriver-wrench'}, -- icons can be found on fontawesome.com/icons (free section)
-    ['2'] = {item = 'weapon_appistol', header = 'AP Pistol', price = 20000, description = 'This could come in handy...', icon = 'fas fa-gun'},
-    ['3'] = {item = 'radio', header = 'Radio', price = 20 , description = 'Just a normal radio', icon = 'fas fa-walkie-talkie'}
+Config.Items = { 
+    label = 'Black Market',
+    slots = 20,
+    items = {
+        [1] = {name = 'advancedlockpick', price = 100, amount = 50, info = {}, type = 'item', slot = 1, header = 'Advanced Lockpick', description = '?', icon = 'fas fa-screwdriver-wrench'}, -- icons can be found on fontawesome.com/icons (free section)
+        [2] = {name = 'weapon_appistol', price = 20000, amount = 50, info = {}, type = 'item', slot = 2, header = 'AP Pistol', description = 'This could come in handy...', icon = 'fas fa-gun'},
+        [3] = {name = 'radio', price = 20, amount = 50, info = {}, type = 'item', slot = 3, header = 'Radio', description = 'Just a normal radio', icon = 'fas fa-walkie-talkie'}
+    }
 }
